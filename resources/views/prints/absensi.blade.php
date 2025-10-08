e57267fc13dafd949d67cd649c31901c0a699116
vickyrodiatululum <vickyrodiatululum24@gmail.com>
2025-10-08 21:03:23 +0700

    revisi laporan absensi

--- resources/views/prints/absensi.blade.php
+++ resources/views/prints/absensi.blade.php
@@ -39,73 +39,162 @@
             height: 37mm;
             background: #fff;
         }
+
+        .driver-info {
+            margin-bottom: 20px;
+            font-size: 11px;
+            padding-top: 25mm;
+        }
+
+        /* Tambahkan style untuk watermark */
+        .watermark {
+            position: fixed;
+            top: 50%;
+            right: -45%;
+            transform: translate(-50%, -50%);
+            opacity: 0.1;
+            z-index: -1;
+            pointer-events: none;
+        }
+
+        .watermark img {
+            width: 400px;
+            /* Sesuaikan ukuran watermark */
+            height: auto;
+        }
+
+        /* Pastikan konten tetap di atas watermark */
+        .content {
+            position: relative;
+            z-index: 1;
+        }
     </style>
 </head>
 
 <body>
-    <div class="header">
-        <img src="{{ public_path('images/header_samarent.jpg') }}" alt="header" class="img-header" />
-        <h2 style="padding-top: 27mm;">Laporan Absensi Driver</h2>
+    <div class="watermark">
+        <img src="{{ public_path('images/icon.png') }}" alt="Watermark">
     </div>
 
-    <div class="driver-info">
-        <table>
-            <tr>
-                <td><strong>Nama Driver:</strong></td>
-                <td>{{ $driver->user->name ?? 'N/A' }}</td>
-            </tr>
-            <tr>
-                <td><strong>Bulan/Month:</strong></td>
-                <td>{{ $month ?? 'N/A' }}</td>
-            </tr>
-        </table>
-    </div>
+    <div class="content">
+        <div class="header">
+            <img src="{{ public_path('images/header_samarent.jpg') }}" alt="header" class="img-header" />
+        </div>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Hari</th>
-                <th>Tanggal</th>
-                <th>No. Polisi</th>
-                <th>KM. Awal</th>
-                <th>KM. Akhir</th>
-                <th>Jam Masuk</th>
-                <th>Jam Cek</th>
-                <th>Jam Keluar</th>
-                <th>Pelanggan</th>
-                <th>End User</th>
-                <th>Approved</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($attendences as $attendance)
+        <div class="driver-info">
+            <table>
                 <tr>
-                    <td>{{ \Carbon\Carbon::parse($attendance->date)->locale('id')->isoFormat('dddd') }}</td>
-                    <td>{{ \Carbon\Carbon::parse($attendance->date)->format('d-m-Y') }}</td>
-                    <td>{{ $attendance->unit ? $attendance->unit->nopol : '-' }}</td>
-                    <td>{{ $attendance->start_km ?? '-' }}</td>
-                    <td>{{ $attendance->end_km ?? '-' }}</td>
-                    <td>{{ $attendance->time_in ? \Carbon\Carbon::parse($attendance->time_in)->format('H:i') : '-' }}
-                    </td>
-                    <td>{{ $attendance->time_check ? \Carbon\Carbon::parse($attendance->time_check)->format('H:i') : '-' }}
-                    </td>
-                    <td>{{ $attendance->time_out ? \Carbon\Carbon::parse($attendance->time_out)->format('H:i') : '-' }}
-                    </td>
-                    <td>{{ $attendance->project->name ?? '-' }}</td>
-                    <td>{{ $attendance->endUser->name ?? '-' }}</td>
-                    <td>{{ $attendance->is_complete ? 'Selesai' : 'Belum' }}</td>
+                    <td><strong>TIME SHEET</strong></td>
+                    <td><strong>: JADWAL KERJA PENGEMUDI (WORKING SCHEDULE FOR DRIVER)</strong></td>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+                <tr>
+                    <td>
+                        <strong>Nama Driver</strong>
+                        <p style="font-style: italic; margin: 2px 0;">Driver Name</p>
+                    </td>
+                    <td style="vertical-align: top;">
+                        <p style="margin: 0;">: {{ $driver->user->name ?? 'N/A' }}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td><strong>Bulan/Month</strong>
+                    </td>
+                    <td>: {{ $month ?? 'N/A' }}</td>
+                </tr>
+            </table>
+        </div>
 
-    <div class="footer" style="margin-top: 30px;">
-        <p>Dicetak pada: {{ now()->format('d-m-Y H:i:s') }}</p>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Hari</th>
+                    <th>Tanggal</th>
+                    <th>No. Polisi</th>
+                    <th>KM. Awal</th>
+                    <th>KM. Akhir</th>
+                    <th>Jam Masuk</th>
+                    <th>Jam Cek</th>
+                    <th>Jam Keluar</th>
+                    <th>Pelanggan</th>
+                    <th>End User</th>
+                    <th>Approved</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($attendences as $attendance)
+                    <tr>
+                        <td>{{ \Carbon\Carbon::parse($attendance->date)->locale('id')->isoFormat('dddd') }}</td>
+                        <td>{{ \Carbon\Carbon::parse($attendance->date)->format('d-m-Y') }}</td>
+                        <td>{{ $attendance->unit ? $attendance->unit->nopol : '-' }}</td>
+                        <td>{{ $attendance->start_km ?? '-' }}</td>
+                        <td>{{ $attendance->end_km ?? '-' }}</td>
+                        <td>{{ $attendance->time_in ? \Carbon\Carbon::parse($attendance->time_in)->format('H:i') : '-' }}
+                        </td>
+                        <td>{{ $attendance->time_check ? \Carbon\Carbon::parse($attendance->time_check)->format('H:i') : '-' }}
+                        </td>
+                        <td>{{ $attendance->time_out ? \Carbon\Carbon::parse($attendance->time_out)->format('H:i') : '-' }}
+                        </td>
+                        <td>{{ $attendance->project->name ?? '-' }}</td>
+                        <td>{{ $attendance->endUser->name ?? '-' }}</td>
+                        <td>{{ $attendance->is_complete ? 'Selesai' : 'Belum' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="footer" style="font-size: 10px;">
+            <p>Dicetak pada: {{ now()->format('d-m-Y H:i:s') }}</p>
+        </div>
+
+        <div style="margin-top: 30px;">
+            <table style="width: 70%; margin: 0 auto;" class="table">
+                <thead>
+                    <tr>
+                        <th style="text-align: center;">Dibuat Oleh</th>
+                        <th style="text-align: center;">Disetujui Oleh</th>
+                        <th style="text-align: center;">SAMARENT</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style="height: 70px; text-align: center; vertical-align: bottom;">
+                            &nbsp;
+                        </td>
+                        <td style="height: 70px; text-align: center; vertical-align: bottom;">
+                            &nbsp;
+                        </td>
+                        <td style="height: 70px; text-align: center; vertical-align: bottom;">
+                            &nbsp;
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td style="">
+                            Nama&nbsp;&nbsp;&nbsp;&nbsp;: <br>
+                            Jabatan&nbsp;: <br>
+                            Tanggal : <br>
+                        </td>
+                        <td style="">
+                            Nama&nbsp;&nbsp;&nbsp;&nbsp;: <br>
+                            Jabatan&nbsp;: <br>
+                            Tanggal : <br>
+                        </td>
+                        <td style="">
+                            Nama&nbsp;&nbsp;&nbsp;&nbsp;: <br>
+                            Jabatan&nbsp;: <br>
+                            Tanggal : <br>
+                        </td>
+                    </tr>
+                </tfoot>
+
+            </table>
+        </div>
     </div>
 
     {{-- halaman baru untuk dokumentasi photo absensi --}}
     <div style="page-break-before: always;">
-        <h3 style="text-align: center; margin-top: 10px;">Dokumentasi Foto Absensi</h3>
+        <h3 style="text-align: center; margin-top: 10px; font-size: 11px;">Dokumentasi Foto Absensi</h3>
         <table class="table">
             <thead>
                 <tr>
