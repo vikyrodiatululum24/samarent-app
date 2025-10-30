c502d8673d48fc338d29164fd12bac5426d14339
viky <vickyrodiatululum24@gmail.com>
2025-10-30 19:13:46 +0700

    perbaiki preview dan print pengajuan

---
+++ app/Models/Finance.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Finance extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'pengajuan_id',
+        'bukti_transaksi',
+    ];
+
+    protected $casts = [
+        'bukti_transaksi' => 'string',
+    ];
+
+    public function pengajuan()
+    {
+        return $this->belongsTo(Pengajuan::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
