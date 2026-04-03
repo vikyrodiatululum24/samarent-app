b8da1a5c91741c2a085047a0cdf66a77d95bd841
viky <vickyrodiatululum24@gmail.com>
2026-04-03 22:31:11 +0700

    perbaiki ajukan methode

--- app/Http/Controllers/PraPengajuanController.php
+++ app/Http/Controllers/PraPengajuanController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Pengajuan;
 use App\Models\PraPengajuan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PraPengajuanController extends Controller
 {
@@ -12,26 +13,33 @@ class PraPengajuanController extends Controller
     {
         $praPengajuan = PraPengajuan::findOrFail($id);
 
-        $pengajuan = new Pengajuan();
-        $pengajuan->user_id = auth()->id();
-        $pengajuan->nama = $praPengajuan->nama_pic;
-        $pengajuan->no_wa = $praPengajuan->no_wa;
-        $pengajuan->project = $praPengajuan->project;
-        $pengajuan->up = $praPengajuan->up;
-        $pengajuan->up_lainnya = $praPengajuan->up_lainnya;
-        $pengajuan->provinsi = $praPengajuan->provinsi;
-        $pengajuan->kota = $praPengajuan->kota;
-        $pengajuan->keterangan = 'PraPengajuan';
-        $pengajuan->keterangan_proses = 'cs';
-        $pengajuan->save();
+        DB::beginTransaction();
+        try {
+            $pengajuan = new Pengajuan();
+            $pengajuan->user_id = auth()->id();
+            $pengajuan->nama = $praPengajuan->nama_pic;
+            $pengajuan->no_wa = $praPengajuan->no_wa;
+            $pengajuan->project = $praPengajuan->project;
+            $pengajuan->up = $praPengajuan->up;
+            $pengajuan->up_lainnya = $praPengajuan->up_lainnya;
+            $pengajuan->provinsi = $praPengajuan->provinsi;
+            $pengajuan->kota = $praPengajuan->kota;
+            $pengajuan->keterangan = 'PraPengajuan';
+            $pengajuan->keterangan_proses = 'cs';
+            $pengajuan->save();
 
-            foreach ($praPengajuan->service_unit as $serviceUnit) {
+            foreach ($praPengajuan->service_units as $serviceUnit) {
                 $serviceUnit->pengajuan_id = $pengajuan->id;
                 $serviceUnit->pra_pengajuan_id = null;
                 $serviceUnit->save();
             }
 
-        $praPengajuan->delete();
+            $praPengajuan->delete();
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return back()->withInput()->withErrors(['error' => 'Terjadi kesalahan saat mengajukan Pra Pengajuan. Silakan coba lagi.']);
+        }
 
         return redirect()->route('filament.admin.resources.pra-pengajuans.index')->with('success', 'Pra Pengajuan berhasil diajukan sebagai Pengajuan.');
     }
